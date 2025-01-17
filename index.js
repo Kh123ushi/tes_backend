0ba1301716b726630a0bb549039ef98b25759f66
Khushi Kumari <khushikr.ug23.ec@nitp.ac.in>
2025-01-17 10:53:26 +0530

    github data added

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const express  = require('express');
 const app = express();
 const port =3000
 
+const githubData = {
+    username: "khushi",
+    repositories: ["repo1"]
+};
+
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
 
 app.get('/about',(req,res)=>{
     res.send('pls login')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT,()=>{
